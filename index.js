6e5fa00afcc1b0507e0e880143e9fea09923be27
Marven Pierre <marvenpierre@Marvens-Laptop.local>
2023-05-17 20:56:44 -0400

    Removing un neccesary item from index

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
-// const express = require('express');
-// const app = express();
-
-// const PORT = process.env.PORT || 5050;
-
-// app.get('/', (req, res) => {
-//     res.send(`<h1>Welcome to my Express App</h1>`);
-// });
-
-// app.listen(PORT, () => {
-//    console.log(`app running on port ${PORT}`)
-// });
-
 const express = require("express");
 const fs = require('fs');
 const app = express();
 const cors = require('cors');
 const commentRoutes = require("./routes/comment");
 require('dotenv').config()
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5050;
 const router = express.Router(); 
 app.use(cors())
 app.use(express.json());
